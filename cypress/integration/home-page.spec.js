8919c4010da3d976e3d67b5f554ce00a5dd83801
Jiaqi Gu <919659233@qq.com>
2020-11-02 22:09:48 +0000

    E2E tests - Home page - Filtering by genre

--- cypress/integration/home-page.spec.js
+++ cypress/integration/home-page.spec.js
@@ -61,8 +61,19 @@ describe("Home Page ", () => {
         cy.get(".card").should("have.length", 0);
       })
     })
-    describe("By movie genre" ,() => {
-      // More later
+    describe("By movie genre", () => {
+      it("should display movies with the specified genre only", () => {
+        const selectedGenreId = 35;
+        const selectedGenreText = "Comedy";
+        const matchingMovies = filterByGenre(movies, selectedGenreId);
+        cy.get("select").select(selectedGenreText); 
+        cy.get(".card").should("have.length", matchingMovies.length);
+        cy.get(".card").each(($card, index) => {
+          cy.wrap($card)
+            .find(".card-title")
+            .should("have.text", matchingMovies[index].title);
+        });      
+      });
     });
   });
 });
