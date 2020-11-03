da4403b0b1bc3c7d6c59e2a3ee5dcd69f377b4fe
Jiaqi Gu <919659233@qq.com>
2020-11-03 01:34:26 +0000

    E2E tests - Navigation - Movies Details page

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -40,11 +40,28 @@ describe("Navigation", () => {
       cy.get("h2").contains("Favorite Movies");
       cy.get("nav").find("li").eq(1).find("a").click();
       cy.url().should("not.include", `/favorites`);
-      cy.get("h2").contains("No. Movies");
+      cy.get("h2").contains("Upcoming Movies");
       cy.get("nav").find("li").eq(2).find("a").click();
       cy.get("nav.navbar-brand").find("a").click();
       cy.url().should("not.include", `/favorites`);
-      cy.get("h2").contains("No. Movies");
+      cy.get("h2").contains("Discover Movies");
+    });
+  });
+  describe("From the Movie Details page ", () => {
+    beforeEach(() => {
+      cy.visit(`/movies/${movieId}`);
+    });
+    it("should change browser URL when show/hide reviews is clicked", () => {
+      cy.contains("Show Reviews").click();
+      cy.url().should("include", `/movies/${movieId}/reviews`);
+      cy.contains("Hide Reviews").click();
+      cy.url().should("not.include", `/movies/${movieId}/reviews`);
+    });
+    it("navigate to the full review page when a 'Full Review' link is clicked", () => {
+        cy.contains("Show Reviews").click();
+        cy.contains("Full Review").click();
+        cy.url().should("include", `/reviews/${reviews[0].id}`);
+
     });
   });
 
